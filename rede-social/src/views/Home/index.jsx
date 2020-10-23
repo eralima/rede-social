e189f8c6357947315db89e0903d49cbcf841f0b6
Rayanne <rayanne_lima2010@hotmail.com>
2020-10-23 14:50:28 -0300

    Estrutura

---
+++ rede-social/src/views/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Home.css"
+
+const Home = () => {
+  return(
+      <main className="Home">
+          {/* <Header nome="Marcus" /> */}
+          {/* <Stories /> */}
+          {/* <Feed /> */}
+          {/* <Suggestions /> */}
+      </main>
+  )
+}
+
+export default Home
