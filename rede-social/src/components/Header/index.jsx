da96a6cccdcc453a385007a71e26381a78f6fe4b
Rayanne <rayanne_lima2010@hotmail.com>
2020-10-23 16:12:41 -0300

    Componente Header - Finalizado

--- rede-social/src/components/Header/index.jsx
+++ rede-social/src/components/Header/index.jsx
@@ -2,38 +2,40 @@ import React from "react"
 import logo from "./../../assets/instagram-logo.png"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faComment, faHeart, faHeartbeat, faHome, faMapMarked, faMapMarkedAlt, faMapMarkerAlt, faSearch, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faComment, faHeart, faHome, faMapMarkerAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 import "./Header.css"
 
 const Header = () => {
 
-
-
     return(
         <>
             <header className="Header">
-                <a href="#" className="logo-link">
-                    <img src={logo} alt="Instagram" className="logo-instagram" />
-                </a>
-                <input type="text" name="search-profiles" id="search-profiles" placeholder="Pesquisar" />
-                <div className="icons">
-                    <a href="#">
-                        <FontAwesomeIcon icon={faHome} />
+                <div className="nav-header">
+                    <a href="Home" className="logo-link">
+                        <img src={logo} alt="Instagram" className="logo-instagram" />
                     </a>
-                    <a href="#">
-                        <FontAwesomeIcon icon={faComment} />
-                    </a>
-                    <a href="#">
-                        <FontAwesomeIcon icon={faMapMarkerAlt} />
-                    </a>
-                    <a href="#">
-                        <FontAwesomeIcon icon={faHeart} />
-                    </a>
-                    <a href="#">
-                        <FontAwesomeIcon icon={faUserCircle} />
-                    </a>
-                </div>
+
+                    <input type="text" name="search-profiles" id="search-profiles" placeholder="Pesquisar" />
+
+                    <div className="icons">
+                        <a href="Home">
+                            <FontAwesomeIcon icon={faHome} />
+                        </a>
+                        <a href="Home">
+                            <FontAwesomeIcon icon={faComment} />
+                        </a>
+                        <a href="Home">
+                            <FontAwesomeIcon icon={faMapMarkerAlt} />
+                        </a>
+                        <a href="Home">
+                            <FontAwesomeIcon icon={faHeart} />
+                        </a>
+                        <a href="Home">
+                            <FontAwesomeIcon icon={faUserCircle} />
+                        </a>
+                    </div>
+                </div>          
             </header>
         </>
     )
